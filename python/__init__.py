cdb83db3000b2bd5702f71f92064d0aaa52242c0
lisa benato <lisa.benato@cern.ch>
2019-06-04 11:11:53 +0200

    issues with libraries

--- python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Analyzer/PFJetTrackVertexAssociation/',1)[0])+'/cfipython/slc6_amd64_gcc630/Analyzer/PFJetTrackVertexAssociation')
