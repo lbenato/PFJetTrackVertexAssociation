c2ba714f428c0fdfc5a2bc6ffce958a09bf7766f
lisa benato <lisa.benato@cern.ch>
2019-07-09 19:38:25 +0200

    More crab utils

---
+++ python/crab_requests_lists.py
@@ -0,0 +1,5 @@
+requests = {
+    'QCD_Pt_15to30_TuneCP5_13TeV_pythia8-v1' : '/QCD_Pt_15to30_TuneCP5_13TeV_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    'QCD_Pt_30to50_TuneCP5_13TeV_pythia8-v1' : '/QCD_Pt_30to50_TuneCP5_13TeV_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    'QCD_Pt_80to120_TuneCP5_13TeV_pythia8-v1' : '/QCD_Pt_80to120_TuneCP5_13TeV_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+}
