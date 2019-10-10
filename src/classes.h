7d9a303e097ee0a9307e50287d336d124439f5eb
lisa benato <lisa.benato@cern.ch>
2019-10-10 18:48:41 +0200

    Modified Jet structure; added PFCandidate structure

--- src/classes.h
+++ src/classes.h
@@ -6,9 +6,11 @@ namespace {
 
     //Structures
     JetType dummy0;
+    PFCandidateType dummy1;
 
     //Vector of structures
     std::vector<JetType> dummyVector0;
+    std::vector<PFCandidateType> dummyVector1;
 
   };
 }
